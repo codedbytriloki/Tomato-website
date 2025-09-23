932802a76a8fd9624534dd00cee01124c38457a1
codedbytriloki <trilokisaini61@gmail.com>
2025-09-23 21:46:41 +0530

    Add Logic

--- Tomato Website/src/App.js
+++ Tomato Website/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Pages/Home/Home'
 import Cart from './Components/Pages/Cart/Cart'
 import PlaceOrder from './Components/Pages/PlaceOrder/PlaceOrder'
 import Footer from './Components/footer/Footer'
+import LoginPopup from './Components/LoginPopup/LoginPopup'
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
